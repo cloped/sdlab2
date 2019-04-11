cd7b010590f80f28ddd9e1d3e8a649d71cecc548
Eduardo Lopes <eduardo.lopes@neemu.com>
2019-04-11 12:23:44 -0400

    add ignore

---
+++ Moeda.java
@@ -0,0 +1,25 @@
+class Moeda {
+  public double cotacao;
+  public String nome;
+  
+  public Moeda(String nome, double cotacao) {
+    this.nome = nome;
+    this.cotacao = cotacao;
+  }
+
+  public void setNome(String nome) {
+    this.nome = nome;
+  }
+
+  public void setCotacao(double cotacao) {
+    this.cotacao = cotacao;
+  }
+
+  public String getNome() {
+    return nome;
+  }
+
+  public double getCotacao() {
+    return cotacao;
+  }
+}
